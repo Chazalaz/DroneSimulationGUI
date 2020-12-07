a579dac4ee8efb0b87c66171c884820fac4e4a73
Chaz <be004779@student.reading.ac.uk>
2020-12-07 14:45:58 +0000

    Creating the setMenu method and running into errors

--- src/DroneGUI/GUI_Interface.java
+++ src/DroneGUI/GUI_Interface.java
@@ -1,13 +1,18 @@
 package DroneGUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,28 +21,30 @@ import javax.swing.border.Border;
 
 public class GUI_Interface extends Application {
     private int canvasSize = 512;
-    Group root;
-    private MyCanvas myCan;
+    Group group;
+    public MyCanvas myCan;
     DroneArena battleArena;
     private VBox infoPane;
     BorderPane bpane;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         primaryStage.setTitle("Drone Simulation");
-        BorderPane bpane = new BorderPane();
+        bpane = new BorderPane();
         bpane.setTop(setMenu());
-        Group root = new Group();
+        group = new Group();
+        //myCan = new MyCanvas(myCan.gc, myCan.xCanvasSize, myCan.yCanvasSize);
         Canvas canvas = new Canvas(canvasSize, canvasSize);
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        root.getChildren().add(canvas);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        group.getChildren().add(canvas);
         myCan = new MyCanvas(canvas.getGraphicsContext2D(), canvasSize, canvasSize);
         battleArena = new DroneArena(myCan.xCanvasSize, myCan.yCanvasSize);
-//        battleArena.addDrone();
+        //battleArena.addDrone();
 //        setMouseEvents();
 //        bpane.setCenter(root);
 //        infoPane = new VBox();
-//        drawStatus();
+////        drawStatus();
 //        bpane.setRight(infoPane);
 //        bpane.setBottom(setButtons());
         primaryStage.setScene(new Scene(root, canvasSize * 2, canvasSize * 1.8));
@@ -55,7 +62,51 @@ public class GUI_Interface extends Application {
     }
 
     MenuBar setMenu() {
-        return null;
+        MenuBar mBar = new MenuBar();
+        Menu mHelp = new Menu();
+        MenuItem help = new MenuItem("Help");
+        help.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                showMessage("Help", "Watch the drones move");
+            }
+        });
+        mHelp.getItems().addAll(help);
+
+        MenuItem mAbout = new MenuItem("About");
+        mAbout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                showAbout();
+            }
+        });
+        mHelp.getItems().addAll(mAbout);
+
+        Menu mFile = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+
+        mFile.getItems().addAll(exit);
+        mBar.getMenus().addAll(mFile, mHelp);
+
+        return mBar;
+    }
+
+    private void showAbout() {
+        showMessage("About", "Pane Demonstrator");
+    }
+
+    private void showMessage(String Title, String Content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(Title);
+        alert.setHeaderText(null);
+        alert.setContentText(Content);
+        alert.showAndWait();
     }
 
 
